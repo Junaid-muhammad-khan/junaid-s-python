0eb29a42ce733e2f874d0b8971b970e4cabc1b62
Junsteel-Legend <imjunaid.mkhan@gmail.com>
2024-07-18 15:14:56 +0300

    Create practice4.py

---
+++ practice4.py
@@ -0,0 +1,21 @@
+# print number 1 to 100
+
+"""num = 1
+while num <= 100:
+    print(num)
+    num += 1
+"""
+# print number 100 to 1
+
+"""num0 = 100
+while num0 >= 1:
+    print(num0)
+    num0 -= 1
+"""
+# print a multiplication table of number n
+
+num = 1
+n = int(input("Enter a number for a multiplication table : "))
+while num <= 10:
+    print(n*num)
+    num += 1
