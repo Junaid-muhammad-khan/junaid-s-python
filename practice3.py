7a5548ceac7af46dc4716e6ecc34b34c51107116
Junsteel-Legend <imjunaid.mkhan@gmail.com>
2024-07-18 13:25:37 +0300

    Update practice3.py

--- practice3.py
+++ practice3.py
@@ -22,9 +22,55 @@ else:
 
 # write a program to count students got A grade in class in tuple
 
-grades = ("A" , "B" , "C" , "f" , "A")
+"""grades = ("A" , "B" , "C" , "f" , "A")
 print(grades.count("A"))
 
 listgrade = ["A" , "C" , "B" , "f" , "A"]
 listgrade.sort()
 print(listgrade)
+"""
+"""word_meaning = {
+    "table" : ("piece of furniture","List of fact and figure"), 
+    "cat" : "animal"
+
+}
+print(word_meaning)
+"""
+# you have given a list of subjects for student. Assume one class room is required for 1 subject.how many classroom are needed by all students
+
+"""subjects = {"html" , "css" , "java" , "jquery " , "python" , "c++","html" , "css" , "java" , "python" , "c++", }
+print(len(subjects))"""
+
+# write a program to enter marks of 3 subjects from user and store them in dictionary. start with and empty dictionary and add one by one. use subject name as key and marks as value
+
+"""marks = {}
+
+marks.update({str(input("Enter subject : ")) : int(input("Enter marks of subject : "))})
+marks.update({str(input("Enter subject : ")) : int(input("Enter marks of subject : "))})
+marks.update({str(input("Enter subject : ")) : int(input("Enter marks of subject : "))})
+print(marks)"""
+
+# Figure a way to store 9 and 9.0 as a different value in a set
+
+a = int(9)
+b = str("9.0")
+aset = {a ,b}
+print(aset)
+
+# OR
+
+aset = {9 ,"9.0"}
+print(aset)
+
+# OR
+
+aset = {"9" ,9.0}
+print(aset)
+
+# or
+
+aset = {
+    ("int" , 9),
+    ("Float" , 9.0)
+    }
+print(aset)
