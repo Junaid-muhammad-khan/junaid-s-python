8bf3d88fc6e1c914b1c35426a65d382fc9e2c584
Junsteel-Legend <imjunaid.mkhan@gmail.com>
2024-07-21 13:58:11 +0300

    Update fun.py

--- fun.py
+++ fun.py
@@ -1,13 +1,13 @@
-from time import time
-from numpy import number
-import phonenumbers
-from phonenumbers import timezone,geocoder,carrier
-number = input("Enter Your number : ")
-phone = phonenumbers.parse(number)
-time = timezone.time_zones_for_number(phone)
-car = carrier.name_for_numbber(phone, "en")
-reg = geocoder.description_for_number(phone, "en")
-print(phone)
-print(time)
-print(car)
-print(reg)
+# from time import time
+# from numpy import number
+# import phonenumbers
+# from phonenumbers import timezone,geocoder,carrier
+# number = input("Enter Your number : ")
+# phone = phonenumbers.parse(number)
+# time = timezone.time_zones_for_number(phone)
+# car = carrier.name_for_numbber(phone, "en")
+# reg = geocoder.description_for_number(phone, "en")
+# print(phone)
+# print(time)
+# print(car)
+# print(reg)
