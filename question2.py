3c18cab87afe831733bb556fcf796d7f247075a2
Junsteel-Legend <imjunaid.mkhan@gmail.com>
2024-07-28 12:59:26 +0300

    Update question2.py

--- question2.py
+++ question2.py
@@ -4,3 +4,12 @@ def divomode(a,b):
     print (divmod(a,b)) # divmode return quotient and reminder as a tuple
     
 divomode(177,10)
+
+#OR
+a = int(input())
+b = int(input())
+c = divmod(a,b)
+
+print(c[0])
+print(c[1])
+print(c)
